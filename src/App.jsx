88e670a910124ce5a94432b6eb37bacdca91c2b0
EzequielCamino <ezeeqii@gmail.com>
2022-08-19 21:55:38 -0300

    Agrega ItemCount provisoriamente (sin cards)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import ItemCount from './components/ItemCount/ItemCount';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
+  const onAdd = (counter)=> {
+    alert("Se agregaron " + counter + " unidades al carrito")
+  }
+
   return (
     <>
     <Navbar/>
     <ItemListContainer greeting='Bienvenido a mi app de React'/>
+    <ItemCount stock={5} initial={0} onAdd={onAdd} />
     </>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+import './ItemCount.css'
+
+function ItemCount({stock, initial, onAdd}) {
+    const [counter, setCounter] = useState(initial);
+    
+        const aumentarCounter = ()=> {
+            counter<stock ? setCounter(counter + 1) : alert('No hay más stock disponible')
+        };
+
+        const disminuirCounter = ()=> {
+            if (counter >0){setCounter(counter - 1)}
+        };
+
+  return (
+    <>
+        <div className='counter'>
+            <button onClick={disminuirCounter} disabled={counter<=0}> - </button>
+            <span>{counter}</span>
+            <button onClick={aumentarCounter}> + </button>
+        </div>
+        <div className='addCart'>
+            <button disabled={counter <=0} onClick={()=>onAdd(counter)}>Agregar al carrito</button>
+        </div>
+    </>
+  )
+}
+
+export default ItemCount
